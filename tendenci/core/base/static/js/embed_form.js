b9c65d7e2d1dd612f45615ffd2e1b5fe739cbc1a
John Eddie Ayson <je@bixly.com>
2012-12-08 08:41:22 +0800

    fixes for fields not being copied to popup fields

--- tendenci/core/base/static/js/embed_form.js
+++ tendenci/core/base/static/js/embed_form.js
@@ -18,11 +18,13 @@ $(document).ready(function() {
         }
         // update dialog fields with fields from the form
         $(".embed-form .form-field").each(function(){
-            $(this).find("input[type='text']").bind("propertychange keyup input paste", function(){
+            //$(this).find("input[type='text']").bind("change propertychange keyup input paste", function(){
+            $(this).find("input[type='text']").change(function(){
                 var id = $(this).attr('id');
                 var modalfield = $(".modal-form-wrap .form-field input[id="+id+"]").val($(this).val());
             });
-            $(this).find("textarea").bind("propertychange keyup input paste", function(){
+            //$(this).find("textarea").bind("change propertychange keyup input paste", function(){
+            $(this).find("textarea").change(function(){
                 var id = $(this).attr('id');
                 var modalfield = $(".modal-form-wrap .form-field textarea[id="+id+"]").val($(this).val());
             });
