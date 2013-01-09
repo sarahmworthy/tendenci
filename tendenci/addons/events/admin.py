81a7b06230e2ebe8f969088e11d4bd875e4a80ef
Jenny Qian <jqian@schipul.com>
2013-01-09 09:54:09 -0600

    Added form_url to the change_view for CustomRegFormAdmin

--- tendenci/addons/events/admin.py
+++ tendenci/addons/events/admin.py
@@ -140,8 +140,9 @@ class CustomRegFormAdmin(admin.ModelAdmin):
 #
 #        return field_list
 
-    def change_view(self, request, object_id, extra_context=None):
-        result = super(CustomRegFormAdmin, self).change_view(request, object_id, extra_context)
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        result = super(CustomRegFormAdmin, self).change_view(request,
+                        object_id, form_url=form_url, extra_context=extra_context)
 
         if not request.POST.has_key('_addanother') and not request.POST.has_key('_continue') and request.GET.has_key('next'):
             result['Location'] = iri_to_uri("%s") % request.GET.get('next')
