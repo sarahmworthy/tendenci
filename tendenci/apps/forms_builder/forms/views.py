457e737aa4a02a922b87133d19cb36d95fbbd960
Eloy Zuniga Jr <eloyz.email@gmail.com>
2013-03-08 12:03:28 -0600

    PEP8 forms_builder/forms/views.py

--- tendenci/apps/forms_builder/forms/views.py
+++ tendenci/apps/forms_builder/forms/views.py
@@ -274,10 +274,10 @@ def entry_detail(request, id, template_name="forms/entry_detail.html"):
     entry = get_object_or_404(FormEntry, pk=id)
 
     # check permission
-    if not has_perm(request.user,'forms.change_form',entry.form):
+    if not has_perm(request.user, 'forms.change_form', entry.form):
         raise Http403
 
-    return render_to_response(template_name, {'entry':entry},
+    return render_to_response(template_name, {'entry': entry},
         context_instance=RequestContext(request))
 
 
