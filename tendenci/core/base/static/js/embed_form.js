27a54a11431630d921572488054c181a6bb83255
John Eddie Ayson <je@bixly.com>
2013-03-12 22:13:28 +0800

    Additional fixes

--- tendenci/core/base/static/js/embed_form.js
+++ tendenci/core/base/static/js/embed_form.js
@@ -1,6 +1,5 @@
-var jq = jQuery.noConflict(true);
-jq(document).ready(function() {
-        jq( ".modal-form-wrap" ).dialog({
+$(document).ready(function() {
+        $( ".modal-form-wrap" ).dialog({
             autoOpen: false,
             height: 400,
             width: 500,
@@ -10,38 +9,38 @@ jq(document).ready(function() {
             resizable: false,
         });
         // check if captcha field exists in the form
-        if (jq(".form-field div.id_captcha").parents("div.embed-form .form-builder-wrap form").length > 0){
-            jq("div.embed-form .form-builder-wrap form .form-field div.id_captcha").hide();
-            jq("div.embed-form .form-builder-wrap form").submit(function(){
-                data = jq(this).parent().attr('data')
-                jq(".modal-form-wrap").each(function(){
-                    if ( jq(this).attr('data') == data ){
-                        jq(this).dialog("open");
+        if ($(".form-field div.id_captcha").parents("div.embed-form .form-builder-wrap form").length > 0){
+            $("div.embed-form .form-builder-wrap form .form-field div.id_captcha").hide();
+            $("div.embed-form .form-builder-wrap form").submit(function(){
+                data = $(this).parent().attr('data')
+                $(".modal-form-wrap").each(function(){
+                    if ( $(this).attr('data') == data ){
+                        $(this).dialog("open");
                     }
                 });
                 return false;
             });
         }
         // update dialog fields with fields from the form
-        jq(".embed-form .form-field").each(function(){
-            //jq(this).find("input[type='text']").bind("change propertychange keyup input paste", function(){
-            jq(this).find("input[type='text']").change(function(){
-                var id = jq(this).attr('id');
-                var modalfield = jq(".modal-form-wrap .form-field input[id="+id+"]").val(jq(this).val());
+        $(".embed-form .form-field").each(function(){
+            //$(this).find("input[type='text']").bind("change propertychange keyup input paste", function(){
+            $(this).find("input[type='text']").change(function(){
+                var id = $(this).attr('id');
+                var modalfield = $(".modal-form-wrap .form-field input[id="+id+"]").val($(this).val());
             });
-            //jq(this).find("textarea").bind("change propertychange keyup input paste", function(){
-            jq(this).find("textarea").change(function(){
-                var id = jq(this).attr('id');
-                var modalfield = jq(".modal-form-wrap .form-field textarea[id="+id+"]").val(jq(this).val());
+            //$(this).find("textarea").bind("change propertychange keyup input paste", function(){
+            $(this).find("textarea").change(function(){
+                var id = $(this).attr('id');
+                var modalfield = $(".modal-form-wrap .form-field textarea[id="+id+"]").val($(this).val());
             });
-            jq(this).find("select").change(function(){
-                var id = jq(this).attr('id');
-                var modalfield = jq(".modal-form-wrap .form-field select[id="+id+"]").val(jq(this).val());
+            $(this).find("select").change(function(){
+                var id = $(this).attr('id');
+                var modalfield = $(".modal-form-wrap .form-field select[id="+id+"]").val($(this).val());
             });
-            jq(this).find("input[type='checkbox']").click(function(){
-                var id = jq(this).attr('id');
-                var checked = jq(this).is(':checked')
-                var modalfield = jq(".modal-form-wrap .form-field input[id="+id+"]").prop('checked', checked);
+            $(this).find("input[type='checkbox']").click(function(){
+                var id = $(this).attr('id');
+                var checked = $(this).is(':checked')
+                var modalfield = $(".modal-form-wrap .form-field input[id="+id+"]").prop('checked', checked);
             });
         });
 });
