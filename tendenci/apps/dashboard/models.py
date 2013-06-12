9dbe325c6b8b11f13a0f27a2a889b06cf39fa3d5
John Eddie Ayson <je@bixly.com>
2013-06-12 14:56:30 +0800

    #379: Dashboard Updates

--- tendenci/apps/dashboard/models.py
+++ tendenci/apps/dashboard/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from tendenci.apps.dashboard.managers import DashboardStatManager
+
+
+class DashboardStat(models.Model):
+    key = models.CharField(max_length=255)
+    value = models.TextField()
+    create_dt = models.DateTimeField(auto_now_add=True)
+
+    objects = DashboardStatManager()
+
+    class Meta:
+        ordering = ('-create_dt',)
--- tendenci/apps/dashboard/views.py
+++ tendenci/apps/dashboard/views.py
@@ -1,3 +1,4 @@
+import simplejson
 from datetime import datetime, timedelta
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response, redirect
@@ -5,13 +6,14 @@ from django.template import RequestContext
 from django.contrib.auth.models import User
 from dateutil import parser
 
+from tendenci.apps.dashboard.models import DashboardStat
 from tendenci.core.event_logs.models import EventLog
 from tendenci.core.site_settings.models import Setting
 from tendenci.core.site_settings.utils import get_setting
 from tendenci.core.theme.shortcuts import themed_response
 
 @login_required
-def index(request, template_name="dashboard/index.html"):
+def index(request, template_name="dashboard/indexV2.html"):
     profile_redirect = get_setting('site', 'global', 'profile_redirect')
     if profile_redirect and profile_redirect != '/dashboard' and not request.user.profile.is_superuser:
         return redirect(profile_redirect)
@@ -40,10 +42,41 @@ def index(request, template_name="dashboard/index.html"):
         if now >= expiration_dt:
             expired = True
 
+    events = simplejson.loads(
+        DashboardStat.objects.get_latest('events_upcoming').value, use_decimal=True)
+    forms = simplejson.loads(
+        DashboardStat.objects.get_latest('forms_30_submissions').value)
+    pages_traffic = simplejson.loads(
+        DashboardStat.objects.get_latest('pages_30_traffic').value)
+    events_traffic = simplejson.loads(
+        DashboardStat.objects.get_latest('events_30_traffic').value)
+    members = simplejson.loads(
+        DashboardStat.objects.get_latest('memberships_30_count').value)
+    corp_new = simplejson.loads(
+        DashboardStat.objects.get_latest('corp_memberships_30_new').value)
+    corp_renew = simplejson.loads(
+        DashboardStat.objects.get_latest('corp_memberships_30_renew').value)
+    corp_expired = simplejson.loads(
+        DashboardStat.objects.get_latest('corp_memberships_30_expired').value)
+    corp_expiring = simplejson.loads(
+        DashboardStat.objects.get_latest('corp_memberships_30_expiring').value)
+    corp_members = simplejson.loads(
+        DashboardStat.objects.get_latest('corp_members_top').value)
+
     EventLog.objects.log()
     return render_to_response(template_name, {
-                                              'has_paid': has_paid,
-                                              'activate_url': activate_url,
-                                              'expired': expired,
-                                              'expiration_dt': expiration_dt
-                                              }, context_instance=RequestContext(request))
+        'has_paid': has_paid,
+        'activate_url': activate_url,
+        'expired': expired,
+        'expiration_dt': expiration_dt,
+        'events': events,
+        'forms': forms,
+        'pages_traffic': pages_traffic,
+        'events_traffic': events_traffic,
+        'members': members,
+        'corp_new': corp_new,
+        'corp_renew': corp_renew,
+        'corp_expired': corp_expired,
+        'corp_expiring': corp_expiring,
+        'corp_members': corp_members,
+    }, context_instance=RequestContext(request))
