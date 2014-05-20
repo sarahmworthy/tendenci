f80f6d74680ffa6cf16256917fe2fe755e51de8b
Jayjay <jayjay@bixly.com>
2014-05-20 22:21:43 +0800

    #889 - Added initial updates to make blog theme compatible

--- tendenci/addons/blog/urls.py
+++ tendenci/addons/blog/urls.py
@@ -1,8 +1,18 @@
 from django.conf.urls.defaults import include, patterns, url
 from tendenci.core.site_settings.utils import get_setting
 
+from zinnia.sitemaps import TagSitemap
+from zinnia.sitemaps import EntrySitemap
+from zinnia.sitemaps import CategorySitemap
+from zinnia.sitemaps import AuthorSitemap
+
 urlpath = get_setting('module', 'blog', 'url')
 enabled = get_setting('module', 'blog', 'enabled') or False
+
+sitemaps = {'tags': TagSitemap,
+            'blog': EntrySitemap,
+            'authors': AuthorSitemap,
+            'categories': CategorySitemap,}
 
 
 urlpatterns = patterns('',
@@ -17,3 +27,10 @@ else:
     urlpatterns += patterns('',
         url(r'^%s/' % urlpath, 'tendenci.addons.blog.views.blog_disabled')
     )
+
+urlpatterns += patterns(
+    'django.contrib.sitemaps.views',
+    url(r'^sitemap.xml$', 'index',
+        {'sitemaps': sitemaps}),
+    url(r'^sitemap-(?P<section>.+)\.xml$', 'sitemap',
+        {'sitemaps': sitemaps}),)
